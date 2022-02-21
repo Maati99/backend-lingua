df35c1b6dcc283f4eec5ede0c89d549c4a2a9bb5
Matias Lingua <matiaslingua@live.com.ar>
2022-02-21 20:49:34 -0300

    Desafio 13 - Servidor con Balance de Carga

---
+++ Desafio 13/utils/productos.js
@@ -0,0 +1,13 @@
+let faker = require('faker/locale/es');
+
+let productos = []
+
+    for(let id = 1;id <=5; id++){
+    productos.push([{
+    id: productos.length + 1,
+    title: faker.commerce.product(),
+    price: faker.commerce.price(),
+    thumbnail: faker.image.image()}])
+}
+
+module.exports = { productos };
