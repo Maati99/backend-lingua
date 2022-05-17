189ba1f3540f3ce02c0ed4d00c9a0ee031d4af21
Maati99 <matiaslingua@live.com.ar>
2022-05-17 20:57:40 -0300

    4° Entrega del Proyecto Final

---
+++ 4° Entrega Proyecto Final/config.js
@@ -0,0 +1,10 @@
+module.export = {
+    mongodb: {
+        cnxStr: 'mongodb+srv://root:coderhouse@cluster0.gbmxl.mongodb.net/ecommerce?retryWrites=true&w=majority',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 1000,
+        }
+    }
+}
